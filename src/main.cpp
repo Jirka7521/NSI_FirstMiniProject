f66f5ef67e5fafcdb0d6b66afefa2ebf05a11ca2
Jiří Majer <jiri.majer@outlook.cz>
2026-03-23 15:27:03 +0100

    Working serial line

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,60 @@
 #include <Arduino.h>
+#include <string>
+
+#include <DHT.h>
+
+#define DHTTYPE DHT11
+const uint8_t DHT_PIN = 13; // GPIO13
+DHT dht(DHT_PIN, DHTTYPE);
 
 // put function declarations here:
-int myFunction(int, int);
+void processInput();
+void initialConnection();
+float readDHT11Temperature();
+bool isConnected = false;
+const char returnConnection[] = "<PONG:PICO_OK>" ;
+const char initConnection[] = "<PING>" ;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  dht.begin();
+  Serial.onReceive(processInput);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void processInput()
+{
+  if(Serial.available())
+  {
+    String input = Serial.readStringUntil('\n');
+    input.trim(); // Remove any leading/trailing whitespace
+    Serial.println(readDHT11Temperature());
+    if (input == initConnection)
+    {
+      initialConnection();
+    }
+    else
+    {
+      Serial.println("Received: " + input);
+    }
+  }
+}
+
+void initialConnection()
+{
+  Serial.println(returnConnection);
+  isConnected = true;
+}
+
+float readDHT11Temperature()
+{
+  float t = dht.readTemperature(); // Celsius
+  if (isnan(t)) {
+    return NAN;
+  }
+  return t;
 }
